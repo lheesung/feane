6adcd174a30236ed697df75618b2ef01002ca0ac
HeeSungLee <a73482006@gmail.com>
2022-09-08 22:29:02 +0900

    ADD::ProductsStruct

--- main/src/App.js
+++ main/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './css/header.css';
+import './css/style.css'
+import './css/header.css'
+import './css/slider.css'
+import './css/products.css'
 import background from './img/background.jpg'
+import product1 from './img/product1.jpg'
+import product2 from './img/product2.jpg'
 
 function App() {
   return (
     <>
-      <Header/>
+      <Header>헤더바</Header>
+      <Slider>메인 슬라이더</Slider>
+      <Products>제품</Products>
     </>
   );
 }
@@ -16,7 +22,7 @@ function Header(){
       <div className='container'>
         <img src={background} alt="background" className='background' />
         <header className='header'>
-          <div className='header-title'><a href="#">Feadne</a></div>
+          <div className='header-title'><a href="#">Feane</a></div>
           <div className='header-menu'>
             <ul id='header-item'>
               <li><a href="#" id='a-hover'>HOME</a></li>
@@ -28,10 +34,35 @@ function Header(){
               <li><a id='a-hover'><i className="fa fa-user" aria-hidden="true"></i></a></li>
               <li><a id='a-hover'><i className="fa fa-shopping-cart" aria-hidden="true"></i></a></li>
               <li><a id='a-hover'><i className="fa fa-search" aria-hidden="true"></i></a></li>
-              <li><button>Order Oline</button></li>
+              <li><button id='order-btn'>Order Oline</button></li>
             </ul>
           </div>
         </header>
+      </div>
+    </>
+    )
+}
+function Slider(){
+  return(
+    <>
+      <div className='slider'>
+        <h1>Fast Food Restaurant</h1>
+        <p>Doloremque, itaque aperiam facilis rerum, commodi, temporibus sapiente ad mollitia laborum quam quisquam esse error unde. Tempora ex doloremque, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.</p>
+        <button id='order-btn'>Order Now</button>
+      </div>
+    </>
+  )
+}
+function Products(){
+  return(
+    <>
+      <div>
+        <div className='box'>
+          <img scr={product1} alt='product' id='product-box'/>
+          <h1>Tasty Thursdays</h1>
+          <h2>20%</h2><h3>off</h3>
+          <button id='order-btn'>Order Now</button>
+        </div>
       </div>
     </>
   )
