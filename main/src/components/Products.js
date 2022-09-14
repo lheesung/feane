5bc744f9da9bdeb4255e1351637ee432c8eeb411
HeeSungLee <a73482006@gmail.com>
2022-09-14 16:52:02 +0900

    ADD::slider.js test

--- main/src/components/Products.js
+++ main/src/components/Products.js
@@ -4,12 +4,14 @@ import product2 from './img/product2.jpg'
 function Products() {
   return (
     <>
-      <div>
+      <div className='product-box'>
         <div className="box">
-          <img scr={product1} alt="product" id="product-box" />
-          <h1>Tasty Thursdays</h1>
-          <h2>20%</h2>
-          <h3>off</h3>
+          <img scr={product1} alt="product" id="product-img" />
+          <div className='product'>
+            <h1>Tasty Thursdays</h1>
+            <h2>20%</h2>
+            <h3>off</h3>
+          </div>
           <button id="order-btn">Order Now</button>
         </div>
       </div>
--- main/src/components/Slider.js
+++ main/src/components/Slider.js
@@ -1,13 +1,57 @@
+let delay = 0;
+
+for(let i=0; i<10; i++){//let i=0; i<3; i++) {
+  if(i===3) {
+    i=0;
+  }
+  delay+=5000; //milli sec
+  setTimeout(() => {
+    for(let j=-30; j<=40; j++) {
+        document.getElementById(`slider${i}`).style.right = `${j}%`;
+        document.getElementById(`slider${i}`).style.transitionTimingFunction = "ease-out";
+        document.getElementById(`slider${i}`).style.transitionDuration = "0.5s";
+    }
+    for(let j=60; j>=-100; j--) {
+      console.log("옆으로 간다");
+   
+      document.getElementById(`slider${Math.abs(i-2)}`).style.left = `${j}%`;
+      document.getElementById(`slider${i}`).style.transitionTimingFunction = "ease-out";
+      document.getElementById(`slider${i}`).style.transitionDuration = "0.4s";
+  }
+  }, delay);
+  delay=0;
+}
+
 function Slider() {
   return (
     <>
-      <div className="slider">
+      <div id="slider0" className="slider">
+        <h1>Fast Food Restaurant</h1>
+        <p>
+          Doloremque, itaque aperiam facilis rerum, commodi, temporibus sapiente
+          ad mollitia laboruwefwefm quam quisquam esse error unde. Tempora ex
+          doloremque, labore, sunt repellat dolore, iste magni quos nihil
+          ducimus libero ipsam.
+        </p>
+        <button id="order-btn">Order Now</button>
+      </div>
+      <div id="slider1" className="slider">
+        <h1>Fast Food Restaurant</h1>
+        <p>
+          Doloremque, itaque apeqwefqwefqweferiam facilis rerum, commodi, temporibus sapiente
+          ad mollitia laborum quam quisquam esse error unde. Tempora ex
+          doloremque, labore, sunt repellat dolore, iste magni quos nihil
+          ducimus libero ipsam.
+        </p>
+        <button id="order-btn">Order Now</button>
+      </div>
+      <div id="slider2" className="slider">
         <h1>Fast Food Restaurant</h1>
         <p>
           Doloremque, itaque aperiam facilis rerum, commodi, temporibus sapiente
           ad mollitia laborum quam quisquam esse error unde. Tempora ex
           doloremque, labore, sunt repellat dolore, iste magni quos nihil
-          ducimus libero ipsam.
+          ducimus liberoqwefqwefqwefqwef ipsam.
         </p>
         <button id="order-btn">Order Now</button>
       </div>
