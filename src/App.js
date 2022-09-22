a2fb53a947609690ffce529972b7c949d58b69f4
HeeSungLee <a73482006@gmail.com>
2022-09-22 23:17:24 +0900

    ADD::Footer struction

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './page/Main.css';
+import './style/style.css';
+import Main from './page/Main'
+
+function App() {
+  return (
+    <Main></Main>
+  );
+}
+
+export default App;
---
+++ src/page/Footer.js
@@ -0,0 +1,15 @@
+function Footer(){
+    return(
+        <>
+        // 26 30 37
+            <footer id='footer-style'>
+                <div id='footer-content'>
+                    <h2>Contect Us</h2>
+                    <i class="fa fa-map-marker" aria-hidden="true"></i><p>Location</p>
+                    <i class="fa fa-phone" aria-hidden="true"></i><p>+01 1234567890</p>
+                    <i class="fa fa-envelope" aria-hidden="true"></i><p>demo@email.com</p>
+                </div>
+            </footer>
+        </>
+    )
+}
